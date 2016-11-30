84def36eefa528ab31f3ff4cc4d3e57cbcbdd592
trippsupp <trippsupp@gmail.com>
2016-11-30 16:05:45 -0500

    Added WelcomeScreen activity.

---
+++ app/src/main/java/com/example/jkast/myapplication/WelcomeScreen.java
@@ -0,0 +1,15 @@
+package com.example.jkast.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+    }
+
+    //public void login
+}
