65f95b64718fabe1170fee7aec4b14420a179d04
jkastan4600 <jkastan4600@gmail.com>
2016-11-30 23:30:42 -0500

    Merge branch 'master' of https://github.com/jkastan4600/MyApplication
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/jkast/myapplication/CreateEventScreen.java
+++ app/src/main/java/com/example/jkast/myapplication/CreateEventScreen.java
@@ -7,14 +7,40 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
-public class CreateEventScreen extends AppCompatActivity {
+public abstract class CreateEventScreen extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+
+    private Spinner spinner;
+    private static final String[] paths = {"item 1", "item 2", "item 3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event_screen);
+
+        spinner = (Spinner) findViewById(R.id.category_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CreateEventScreen.this,
+                android.R.layout.simple_spinner_item, paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
     }
 
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
 }
