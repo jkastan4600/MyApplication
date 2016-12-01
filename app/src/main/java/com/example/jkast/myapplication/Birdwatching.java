ecafcb5a9de59175f51411b8ddc6ad514ffde503
trippsupp <trippsupp@gmail.com>
2016-11-30 22:44:46 -0500

    shi56

--- app/src/main/java/com/example/jkast/myapplication/Birdwatching.java
+++ app/src/main/java/com/example/jkast/myapplication/Birdwatching.java
@@ -1,13 +1,27 @@
 package com.example.jkast.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import static android.widget.Toast.makeText;
 
 public class Birdwatching extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birdwatching);
+    }
+
+    public void attendButton(View view){
+        makeText(Birdwatching.this,"You are scheduled to attend this event.", Toast.LENGTH_LONG).show();
+        Intent returnToMainMenu = new Intent(this,MainMenu.class); // switches to it main menu
+        startActivity(returnToMainMenu);
     }
 }
