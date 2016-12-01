7a17fdb058eb2c6747ac3ba5f1d70dc74b40b998
trippsupp <trippsupp@gmail.com>
2016-11-30 21:26:04 -0500

    shi56

--- app/src/main/java/com/example/jkast/myapplication/FindEvents.java
+++ app/src/main/java/com/example/jkast/myapplication/FindEvents.java
@@ -1,7 +1,9 @@
 package com.example.jkast.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindEvents extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class FindEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_events);
+    }
+
+    public void birdwatching(View view){
+        Intent birdwatchingButton = new Intent(this,Birdwatching.class); // switches to birdwatching
+        startActivity(birdwatchingButton);
+    }
+
+    public void itSeminar(View view){
+        Intent itSeminarButton = new Intent(this,IT_Seminar.class); // switches to it seminar
+        startActivity(itSeminarButton);
     }
 }
--- app/src/main/java/com/example/jkast/myapplication/MainMenu.java
+++ app/src/main/java/com/example/jkast/myapplication/MainMenu.java
@@ -1,7 +1,9 @@
 package com.example.jkast.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+
+    public void findEventsButton(View view){
+        Intent findEventsButton = new Intent(this,FindEvents.class); // switches to Find Events
+        startActivity(findEventsButton);
     }
 }
