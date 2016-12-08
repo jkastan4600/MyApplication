775fc7771e6e16fc8ee3dc1532efac816be73c6c
trippsupp <Cantguessth1slol>
2016-12-07 21:17:00 -0500

    User authentication successfully implemented.

--- app/src/main/java/com/example/jkast/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/jkast/myapplication/LoginActivity.java
@@ -325,15 +325,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 return false;
             }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
             return true;
         }
 
